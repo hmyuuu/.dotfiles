2f7ef6cc84788eb7a4d6f5b7d2a825fb2743ced4
Huaiming Yu <huaimingyuuu@gmail.com>
2023-09-29 17:49:51 +0800

    add tmux

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -10,3 +10,13 @@ eval /Users/hmyuuu/miniconda/bin/conda "shell.fish" "hook" $argv | source
 alias pluto="julia --project=. -e 'using Pluto; Pluto.run()'"
 
 set -Ux JULIA_EDITOR code
+set -Ux EDITOR nvim
+set -Ux NVIM_CONF ~/.config/nvim/
+set -Ux FISH_CONF ~/.config/fish/
+set -Ux TMUX_CONF ~/.config/tmux/
+
+set -Ux fish_tmux_config $HOME/.tmux.conf
+set -Ux fish_tmux_autostart false
+set -Ux fish_tmux_autostart_once  true
+set -Ux fish_tmux_unicode true
+set -Ux fish_tmux_autoconnect false
