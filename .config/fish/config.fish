d77e41b0fd457d8363ab3f61ae759e4d28b6034b
Huaiming Yu <huaimingyuuu@gmail.com>
2023-10-03 16:52:42 +0800

    fix: sed in macos -> gnu-sed

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,6 +8,7 @@ eval /Users/hmyuuu/miniconda/bin/conda "shell.fish" "hook" $argv | source
 # <<< conda initialize <<<
 
 alias pluto="julia --project=. -e 'using Pluto; Pluto.run()'"
+alias sed=gsed
 
 set -Ux JULIA_EDITOR code
 set -Ux EDITOR nvim
@@ -20,3 +21,13 @@ set -Ux fish_tmux_autostart false
 set -Ux fish_tmux_autostart_once  true
 set -Ux fish_tmux_unicode true
 set -Ux fish_tmux_autoconnect false
+
+
+set -Ux HOMEBREW_API_DOMAIN "https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles/api"
+set -Ux HOMEBREW_BOTTLE_DOMAIN "https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles"
+set -Ux HOMEBREW_BREW_GIT_REMOTE "https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/brew.git"
+set -Ux HOMEBREW_CORE_GIT_REMOTE "https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-core.git"
+set -Ux HOMEBREW_PIP_INDEX_URL "https://pypi.tuna.tsinghua.edu.cn/simple"
+
+# set -x PATH /usr/local/opt/gnu-sed/libexec/gnubin $PATH
+
